e00f2b2a8a9228f2cdabf6d4a8b76a384032f758
MADJIHADx <madjihad1945@gmail.com>
2015-06-28 23:36:16 -0700

    clean up on isle 4

--- madhudspeed.lua
+++ madhudspeed.lua
@@ -8,86 +8,83 @@ function madhudspeed:initialize()
 
 	CheckSetDefaultValue(self, "userData", "table", {});
 	CheckSetDefaultValue(self.userData, "upsIncrement", "number", 50);
-	color = Color(125,255,125,180)
+	local color = Color(125,255,125,180);
 	CheckSetDefaultValue(self.userData, "bColor", "table", color);
-	color = Color(255,255,255,220)
+	local color = Color(255,255,255,220);
 	CheckSetDefaultValue(self.userData, "nColor", "table", color);
-end
+end;
 
 function clampToNoDecimal(n)
 	return math.floor(n * 1) / 1;
-end
+end;
 
 function madhudspeed:drawOptions(x, y)
-	ygap = 40
-
 	uiLabel("Speed Display Increment:", x, y);
+	local ygap = 40;
 	local Width = 200;
 	local Start = 240;
 	local user = self.userData;
 	user.upsIncrement = clampToNoDecimal(uiEditBox(user.upsIncrement, x + Start + Width + 10, y, 60));
 
-	y = y + ygap;
-
+	local y = y + ygap;
 	-----------------------------------------------------------
 	---------------copy pasta from aliasedfrog-----------------
 	-----------------------------------------------------------
-
 	uiLabel("Bars Colour:", x, y);
 	local user = self.userData;
 	local col = user.color;
 	user.bColor = user.bColor or {r = col.r, g = col.g, b = col.b, a = col.a};
 	user.bColor = uiColorPicker(x + 15,y + ygap, user.bColor,{});
 
-	y = y + 200 + ygap;
+	local y = y + 200 + ygap;
 
 	uiLabel("Numbers Colour:", x, y);
 	local user = self.userData;
 	local col = user.color;
 	user.nColor = user.nColor or {r = col.r, g = col.g, b = col.b, a = col.a};
 	user.nColor = uiColorPicker(x + 15,y + ygap, user.nColor,{});
-
 	-------------------------------------------------------------
 	---------------end copy pasta from aliasedfrog---------------
 	-------------------------------------------------------------
-
 	saveUserData(user);
-end
+end;
 
 registerWidget("madhudspeed");
-testspeed = 0
+testspeed = 0;
 
-function madhudspeed:draw() --A lot of this stuff probably should be before draw()
+function madhudspeed:draw() --A lot of this stuff probably should be before draw() maybe??
 	if not shouldShowHUD() or not isRaceMode() then return end;
-	local speedscale = 1 --scale the Y stuff
+	local speedscale = 1; --scale the Y stuff
 
 	--Make these available in the widget prefs
-	local topy = 400 * speedscale--Bounding Box  --self.userData.topy
-	local bottomy = 0 --Bounding Box (always 0?)
-	local speedmetercolor = self.userData.bColor
-	local textcolour = self.userData.nColor
-	local yoffset = topy/2 --center the bars on the y axis
-	local bartotextxoffset = 5
-	local lerpspeedscale = 30
-	local lerpspeed = clamp(lerpspeedscale * deltaTimeRaw, 0.0001, 1) --maybe add easing, like fast in slow out?
+	local topy = 400 * speedscale; --Bounding Box
+	local bottomy = 0; --Bounding Box (always 0?)
+	local speedmetercolor = self.userData.bColor; --done!
+	local textcolour = self.userData.nColor; --done!
+	local yoffset = topy/2; --center the bars on the y axis
+	local bartotextxoffset = 5;
+	local lerpspeedscale = 30;
+	local lerpspeed = clamp(lerpspeedscale * deltaTimeRaw, 0.0001, 1); --maybe add easing, like fast in slow out?
 
 	--precision speed text indicator box params
-	local boxheight = 20
-	local boxwidth = 51.5 --maybe make this dynamic or scissor it for 9999+
-	local boxoffset = 10
+	local boxheight = 20;
+	local boxwidth = 51.5; --maybe make this dynamic or scissor it for 9999+
+	local boxoffset = 10;
 
 	--static helpers
-	local player = getPlayer()
-	local speed = math.ceil(player.speed)
-	testspeed = lerp(testspeed, speed, lerpspeed)
-	local lerpdspeed = round(testspeed)
-	local ylocation = lerpdspeed * speedscale --the Y location changes by speed
+	local player = getPlayer();
+	local speed = math.ceil(player.speed);
+	testspeed = lerp(testspeed, speed, lerpspeed);
+	local lerpdspeed = round(testspeed);
+	local ylocation = lerpdspeed * speedscale; --the Y location changes by speed
 
-	local linewidth = 25
-	local linewidthhalf = linewidth/2 --used to centre lines on the x axis
-	local lineheight = 2.5
-	local upsincpref = self.userData.upsIncrement
-	local upsincrement = upsincpref * speedscale
+	local linewidth = 25;
+	local linewidthhalf = linewidth/2; --used to centre lines on the x axis
+	local linewidthsub = linewidth/5; --used to centre lines on the x axis
+
+	local lineheight = 2.5;
+	local upsincpref = self.userData.upsIncrement;
+	local upsincrement = upsincpref * speedscale;
 
 	nvgBeginPath();
 	nvgRect(linewidth/2 + boxoffset, -boxheight/2, boxwidth, boxheight);
@@ -106,19 +103,18 @@ function madhudspeed:draw() --A lot of this stuff probably should be before draw
 	for bardrawloop = (bottomy + -topy), ylocation, upsincrement do
 		if (ylocation - bardrawloop) <= topy and (ylocation - bardrawloop) >= bottomy then do
 			nvgBeginPath();
-			nvgRect(-linewidthhalf, (ylocation + -yoffset - bardrawloop), linewidth, 0);
+			nvgMoveTo(-linewidthhalf,(ylocation + -yoffset - bardrawloop));
+			nvgLineTo(linewidthhalf, (ylocation + -yoffset - bardrawloop));
 			nvgStrokeColor(speedmetercolor);
 			nvgStrokeWidth(lineheight);
 			nvgStroke();
-
 			nvgFontSize(18);
 			nvgFontFace("Volter__28Goldfish_29");
 			nvgTextAlign(NVG_ALIGN_RIGHT, NVG_ALIGN_MIDDLE);
 			nvgFillColor(textcolour);
 			nvgText(-linewidthhalf + -bartotextxoffset, (ylocation + -yoffset - bardrawloop), bardrawloop + yoffset);
-		end
-	end
-end
-end
-
+		end;
+	end;
+end;
+end;
 --Thanks to Qualx, AliasedFrog, Bonuspunkt and other dudes I forgot to mention in #reflex for helping me with lua questions.
