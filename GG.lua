f2428bc65505bdd17425cf776e2b428750c724cc
MADJIHADx <madjihad1945@gmail.com>
2016-12-01 17:17:29 -0800

    Lots of fixes and added options.

--- GG.lua
+++ GG.lua
@@ -2,19 +2,46 @@ require "base/internal/ui/reflexcore"
 GG =
 {
   canPosition = false,
-};
+}
 
 registerWidget("GG")
 
 function GG:initialize()
-  oldGameState = world.gameState
-  glhfSaid = 0
-  ggSaid = 0
+  self.userData = loadUserData()
+  CheckSetDefaultValue(self, "userData", "table", {})
+  CheckSetDefaultValue(self.userData, "ggMessage", "string", "gg")
+  CheckSetDefaultValue(self.userData, "glhfMessage", "string", "glhf")
+  -----------------------------
+  oldGameState = 0
+  glhfSaid = false
+  ggSaid = false
   messageTimerActive = false
   ggTimer = 0
   delayTime = 2
-  messageToSend = "test gg"
+  messageToSend = "nil"
   countDown = 0
+end
+
+function GG:drawOptions(x, y)
+  local optargs = {};
+  optargs.intensity = intensity
+  local user = self.userData
+
+  ui2Label("GG Message", x, y, optargs)
+  user.ggMessage = string.format("%s", ui2EditBox(user.ggMessage, 25, y, 125, optargs))
+
+  local y = y + 60
+
+  ui2Label("GLHF Message", x, y, optargs)
+  user.glhfMessage = string.format("%s", ui2EditBox(user.glhfMessage, 25, y, 125, optargs))
+
+  if user.ggMessage == nil or user.ggMessage == "" then
+    user.ggMessage = "gg"
+  elseif user.glhfMessage == nil or user.glhfMessage == "" then
+    user.glhfMessage = "glhf"
+  end
+
+  saveUserData(user)
 end
 
 function GG:draw()
@@ -24,35 +51,39 @@ function GG:draw()
   local gameMode = gamemodes[world.gameModeIndex]
   if world == nil then return end
   if gameMode.shortName ~= "tdm" and gameMode.shortName ~= "ctf" and gameMode.shortName ~= "1v1" and gameMode.shortName ~= "ffa" then return end
+  local user = self.userData
 
+  -----------------------------
   if messageTimerActive then
-  local ggTimer = ggTimer + deltaTimeRaw
+    ggTimer = ggTimer + deltaTimeRaw
+  else ggTimer = 0
   end
-
+  -----------------------------
   if world.timerActive then
-  local countDown = countDown + deltaTimeRaw
+    countDown = countDown + deltaTimeRaw
   else countDown = 0
   end
+  -----------------------------
 
-  if countDown < delayTime and world.gameState == GAME_STATE_WARMUP and glhfSaid == 0 then
-  local messageToSend = "glhf"
-  local glhfSaid = 1
-  local timerActive = true
-  local ggTimer = 0
-    --Maybe use GAME_STATE_GAMEOVER instead of GAME_STATE_ROUNDCOOLDOWN_SOMEONEWON
-  elseif oldGameState == GAME_STATE_ROUNDACTIVE and world.gameState == GAME_STATE_GAMEOVER and ggSaid == 0 then
-  local messageToSend = "gg"
-  local ggSaid = 1
-  local messageTimerActive = true
-  local ggTimer = 0
+  if countDown > 0 and countDown < delayTime and world.gameState == GAME_STATE_WARMUP and glhfSaid == false then
+    messageToSend = string.format("say %s", user.glhfMessage)
+    glhfSaid = true
+    messageTimerActive = true
   end
-  local oldGameState = world.gameState
 
-  if ggTimer >= delayTime  then
-    consolePrint("say "..messageToSend)
-  local glhfSaid = 0
-  local ggSaid = 0
-  local messageTimerActive = false
-  local ggTimer = 0
+  if oldGameState == GAME_STATE_ACTIVE and world.gameState == GAME_STATE_GAMEOVER and ggSaid == false then
+    messageToSend = string.format("say %s", user.ggMessage)
+    ggSaid = true
+    messageTimerActive = true
+  end
+
+  oldGameState = world.gameState
+
+  if ggTimer >= delayTime then
+    consolePerformCommand(messageToSend)
+    glhfSaid = false
+    ggSaid = false
+    messageTimerActive = false
+    ggTimer = 0
   end
 end
